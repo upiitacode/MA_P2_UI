aa01015b5fdf3483bc40b42d532b53478566edaa
Manuel Limones <limonesu.me@gmail.com>
2015-11-29 00:32:28 -0600

    Update keil project

--- mdk/RTE/RTE_Components.h
+++ mdk/RTE/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'Cpp_03_RTOS' 
+ * Project: 'Cpp_08_SyncHardware' 
  * Target:  'Target 1' 
  */
 
