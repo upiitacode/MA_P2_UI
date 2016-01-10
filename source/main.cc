f0f0d3e232e9df48ed2002ba51000afb02485d3a
Manuel Limones <limonesu.me@gmail.com>
2016-01-10 11:56:59 -0600

    Add plotting function

--- source/main.cc
+++ source/main.cc
@@ -4,6 +4,7 @@
 #include "safe_stdlib.h"
 #include "board.h"
 #include "tft_driver.h"
+#include <math.h>
 
 void tarea1(void const * arguments); //tarea 1
 osThreadId  tarea1ID;	//identificador del hilo tarea 1
@@ -39,11 +40,45 @@ void tarea1Init(void){
 	tarea1ID= osThreadCreate(osThread(tarea1),NULL);
 }
 
+void TFT_Plot(unsigned short x, unsigned short xlen, unsigned short y, unsigned short ylen, const int* pData, int length){
+	TFT_Box(x,y-ylen,x+xlen,y,Black);
+	TFT_H_Line(x,x+xlen,y,White);
+	TFT_V_Line(y-ylen,y,x,White);
+	for(int i = 0; i < length; i++){
+		TFT_Dot(x+i+1,y-pData[i]-1,Yellow);
+	}
+}
+
+int get_sine(void){
+	static int i = 0;
+	return(sin(i++*0.05*2*3.1416)*0.5+0.5)*48;
+	
+}
+#define SCOPE_BUFFER_SIZE 128
+#define SCOPE_POS_X 10
+#define SCOPE_POS_Y 80
+int scope_buffer[SCOPE_BUFFER_SIZE];
+int scope_buffer_index = 0;
+void ScopeInit(){
+	TFT_Plot(SCOPE_POS_X,150,SCOPE_POS_Y,50,scope_buffer,SCOPE_BUFFER_SIZE);
+}
+
+void ScopeUpdate(int Datos){
+	TFT_Dot(SCOPE_POS_X+scope_buffer_index+1,SCOPE_POS_Y-scope_buffer[scope_buffer_index]-1,Black);
+	scope_buffer[scope_buffer_index] = Datos;
+	TFT_Dot(SCOPE_POS_X+scope_buffer_index+1,SCOPE_POS_Y-scope_buffer[scope_buffer_index]-1,Yellow);
+	scope_buffer_index++;	
+	scope_buffer_index %= SCOPE_BUFFER_SIZE;
+	
+	
+}
+
 void tarea1(void const * arguments){
-	TFT_Fill(Black);
+	TFT_Fill(Blue);	
+	TFT_Text("Hello, World!",10,10,8,Yellow,Blue);
+	ScopeInit();
 	while(1){
-		TFT_Fill(Black);
-		TFT_Text("Hello, World!",10,10,8,Green,Black);
-		osDelay(1000);
+		ScopeUpdate(get_sine());
+		osDelay(10);
 	}
 }
